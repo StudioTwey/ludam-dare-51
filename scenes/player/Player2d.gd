3851a2ed404369adcb33ab9d8de84819525edd43
Will <56137739+wpank84@users.noreply.github.com>
2022-09-30 22:39:27 -0500

    updated player2d
    better movement, added projectiles

---
+++ scenes/player/Player2d.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+
+@export var speed = 1000.0
+
+func get_input():
+	velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		#$Sprite.play("run")
+		#$Sprite.flip_h = false
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+		#$Sprite.play("run")
+		#$Sprite.flip_h = true
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+		#$Sprite.play("run")
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+		#$Sprite.play("run")
+	
+	#if velocity.y == 0 and velocity.x == 0:
+	# $Sprite.play("idle")
+	# Stops diagnals from being faster
+	velocity = velocity.normalized() * speed
+
+
+
+func _physics_process(_delta):
+	get_input()
+	
+	move_and_slide()
