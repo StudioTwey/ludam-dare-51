b86f1dc7fcb63bb0f9d78d9e52d2b9cca01ac19c
Will <56137739+wpank84@users.noreply.github.com>
2022-10-02 12:46:12 -0500

    updated player and enemy entities
    - Added assets for the player and resized collision boxes
    - Added two new enemy types

--- scenes/player/Projectile.gd
+++ scenes/player/Projectile.gd
@@ -4,19 +4,19 @@ extends Area2D
 @export var max_time = 0.5
 
 func start_timer():
-	var timer = Timer.new()
-	timer.name = "Timer"
-	timer.one_shot = true
-	timer.wait_time = max_time
-	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
-	add_child(timer)
-	timer.start()
-	
+  var timer = Timer.new()
+  timer.name = "Timer"
+  timer.one_shot = true
+  timer.wait_time = max_time
+  timer.connect("timeout", Callable(self, "_on_timer_timeout"))
+  add_child(timer)
+  timer.start()
+  
 func _on_timer_timeout():
-	queue_free()
+  queue_free()
 
 func _ready():
-	start_timer()
+  start_timer()
 
 func _physics_process(delta):
-	position += transform.x * speed * delta
+  position += transform.x * speed * delta
